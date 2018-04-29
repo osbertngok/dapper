22878784b1bc1aabad645c1cbf2d42f2671286ce
Osbert Ngok <osbertngok@gmail.com>
2018-04-29 23:06:00 +0800

    minimal typescript project with a sidebar

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,58 @@
 import Vue from 'vue';
-import App from './App.vue';
 import router from './router';
 import store from './store';
 
+import Vuetify from 'vuetify/es5/components/Vuetify';
+import VApp from 'vuetify/es5/components/VApp';
+import VGrid from 'vuetify/es5/components/VGrid';
+import VCard from 'vuetify/es5/components/VCard';
+import VToolbar from 'vuetify/es5/components/VToolbar';
+import VTooltip from 'vuetify/es5/components/VTooltip';
+import VBtn from 'vuetify/es5/components/VBtn';
+import VIcon from 'vuetify/es5/components/VIcon';
+import VForm from 'vuetify/es5/components/VForm';
+import VTextField from 'vuetify/es5/components/VTextField';
+import VNavigationDrawer from 'vuetify/es5/components/VNavigationDrawer';
+import VList from 'vuetify/es5/components/VList';
+import VDivider from 'vuetify/es5/components/VDivider';
+import VSubheader from 'vuetify/es5/components/VSubheader';
+import VMenu from 'vuetify/es5/components/VMenu';
+import VDataIterator from 'vuetify/es5/components/VDataIterator';
+import VDialog from 'vuetify/es5/components/VDialog';
+import VSelect from 'vuetify/es5/components/VSelect';
+import VSwitch from 'vuetify/es5/components/VSwitch';
+import VCheckbox from 'vuetify/es5/components/VCheckbox';
+import VSnackbar from 'vuetify/es5/components/VSnackbar';
+import VExpansionPanel from 'vuetify/es5/components/VExpansionPanel';
+import transitions from 'vuetify/es5/components/transitions';
+
+import App from './App.vue';
+
+Vue.use(Vuetify, {
+    components: {
+        VApp,
+        VGrid,
+        VCard,
+        VToolbar,
+        VTooltip,
+        VBtn,
+        VIcon,
+        VForm,
+        VTextField,
+        VNavigationDrawer,
+        VDivider,
+        VList,
+        VSubheader,
+        VMenu,
+        VDataIterator,
+        VDialog,
+        VSelect,
+        VSwitch,
+        VCheckbox,
+        VSnackbar,
+        VExpansionPanel,
+        transitions,
+    }});
 Vue.config.productionTip = false;
 
 new Vue({
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,58 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+  <v-app>
+    <v-navigation-drawer>
+      <v-toolbar flat>
+        <v-list>
+          <v-list-tile>
+            <v-list-tile-title class="title">
+              Application
+            </v-list-tile-title>
+          </v-list-tile>
+        </v-list>
+      </v-toolbar>
+      <v-divider></v-divider>
+      <v-list dense class="pt-0">
+        <v-list-tile v-for="item in items" :key="item.title" @click="">
+          <v-list-tile-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-content>
+            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          </v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+  </v-app>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+
+interface IMenuItem {
+    title: string;
+    icon: string;
+}
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+
+    public items: IMenuItem[];
+
+    constructor() {
+        super();
+        this.items = [
+            {
+                title: 'Home',
+                icon: 'dashboard',
+            },
+            {
+                title: 'About',
+                icon: 'question_answer',
+            },
+        ];
+    }
+}
 </script>
